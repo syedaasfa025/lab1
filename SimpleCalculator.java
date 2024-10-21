33ee7302fef7739e1f0a717ec8ed33003664fd87
syedaasfa025 <161472449+syedaasfa025@users.noreply.github.com>
2024-10-21 15:48:39 +0500

    Create SimpleCalculator.java

---
+++ SimpleCalculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter second number:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Select operation: +, -, *, /");
+        char operation = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error! Division by zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation!");
+                return;
+        }
+
+        System.out.printf("Result: %.2f%n", result);
+    }
+}
